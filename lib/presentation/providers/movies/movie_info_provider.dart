763704c482530bbdb7f14953877408573559fc4c
Duvan Mendoza <duvanmendoza01@gmail.com>
2024-08-02 12:03:05 -0600

    Sección 14: Películas individuales y actores / Actores de la película

--- lib/presentation/providers/movies/movie_info_provider.dart
+++ lib/presentation/providers/movies/movie_info_provider.dart
@@ -22,7 +22,6 @@ class MovieMapNotifier extends StateNotifier<Map<String,Movie>> {
   Future<void> loadMovie(String movieId) async {
 
     if(state[movieId] != null) return;
-    print('Realizando peticion HTTP');
 
     final movie = await getMovie(movieId);
 
