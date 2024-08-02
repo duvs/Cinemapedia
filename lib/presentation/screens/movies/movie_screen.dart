4ff83c97ad34a53e340035e38f297fb2a9cc9046
Duvan Mendoza <duvanmendoza01@gmail.com>
2024-08-02 17:08:46 -0600

    Sección 14: Películas individuales y actores / Mostrar los actores de la película

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -127,12 +127,73 @@ class _MovieDetails extends StatelessWidget {
           ),
         ),
 
-        const SizedBox(height: 100,),
+        _ActorsByMovie(movieId: movie.id.toString()),
+
+        const SizedBox(height: 50,),
 
       ],
     );
   }
 }
+
+class _ActorsByMovie extends ConsumerWidget {
+
+  final String movieId;
+
+  const _ActorsByMovie({required this.movieId});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final actorsByMovie = ref.watch(actorsByMovieProvider);
+
+    if(actorsByMovie[movieId] == null) {
+      return const CircularProgressIndicator(strokeWidth: 2,);
+    }
+
+    final actors = actorsByMovie[movieId]!;
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: actors.length,
+        itemBuilder: (context, index) {
+          final actor = actors[index];
+
+          return Container(
+            padding: const EdgeInsets.all(8.0),
+            width: 135,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                    actor.profilePath,
+                    height: 180,
+                    width: 135,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+
+                const SizedBox(height: 5,),
+
+                Text(actor.name, maxLines: 2,),
+                Text(actor.character ?? '', maxLines: 2,
+                  style: const TextStyle(fontWeight: FontWeight.bold, overflow: TextOverflow.ellipsis),
+                ),
+
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
 
 class _CustomSliverAppBar extends StatelessWidget {
 
@@ -151,32 +212,32 @@ class _CustomSliverAppBar extends StatelessWidget {
       foregroundColor: Colors.white,
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        title: Text(
-          movie.title,
-          style: const TextStyle(fontSize: 20, color: Colors.white),
-          textAlign: TextAlign.start,
+        // title: Text(
+        //   movie.title,
+        //   style: const TextStyle(fontSize: 20, color: Colors.white),
+        //   textAlign: TextAlign.start,
           
-        ),
+        // ),
         background: Stack(
           children: [
             SizedBox.expand(
               child: Image.network(movie.posterPath, fit: BoxFit.cover,),              
             ),
 
-            const SizedBox.expand(
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    stops: [0.7, 1.0],
-                    colors: [
-                      Colors.transparent, 
-                      Colors.black87,
-                    ])
-                ),
-              ),
-            ),
+            // const SizedBox.expand(
+            //   child: DecoratedBox(
+            //     decoration: BoxDecoration(
+            //       gradient: LinearGradient(
+            //         begin: Alignment.topCenter,
+            //         end: Alignment.bottomCenter,
+            //         stops: [0.7, 1.0],
+            //         colors: [
+            //           Colors.transparent, 
+            //           Colors.black87,
+            //         ])
+            //     ),
+            //   ),
+            // ),
 
             const SizedBox.expand(
               child: DecoratedBox(
