94e74871f47a4e80c293a17c8de0477d23dec4a1
Duvan Mendoza <duvanmendoza01@gmail.com>
2024-08-01 20:57:19 -0600

    Sección 14: Películas individuales y actores / MovieDB to Entity

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,12 +1,13 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/themoviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/themoviedb/movie_themoviedb.dart';
 
 class MovieMapper {
   static Movie theMovieDBToEntity(MovieTheMovieDB themoviedb) => Movie(
       adult: themoviedb.adult,
-      backdropPath: themoviedb.backdropPath != '' 
-        ? 'https://image.tmdb.org/t/p/w500${themoviedb.backdropPath}'
-        : 'https://programacion.net/files/article/20161110041116_image-not-found.png',
+      backdropPath: themoviedb.backdropPath != ''
+          ? 'https://image.tmdb.org/t/p/w500${themoviedb.backdropPath}'
+          : 'https://programacion.net/files/article/20161110041116_image-not-found.png',
       genreIds: themoviedb.genreIds.map((e) => e.toString()).toList(),
       id: themoviedb.id,
       originalLanguage: themoviedb.originalLanguage,
@@ -14,8 +15,28 @@ class MovieMapper {
       overview: themoviedb.overview,
       popularity: themoviedb.popularity,
       posterPath: themoviedb.posterPath != ''
-        ? 'https://image.tmdb.org/t/p/w500${themoviedb.posterPath}'
-        : 'no-poster',
+          ? 'https://image.tmdb.org/t/p/w500${themoviedb.posterPath}'
+          : 'no-poster',
+      releaseDate: themoviedb.releaseDate,
+      title: themoviedb.title,
+      video: themoviedb.video,
+      voteAverage: themoviedb.voteAverage,
+      voteCount: themoviedb.voteCount);
+
+  static Movie movieDetailsToEntity(MovieDetails themoviedb) => Movie(
+      adult: themoviedb.adult,
+      backdropPath: themoviedb.backdropPath != ''
+          ? 'https://image.tmdb.org/t/p/w500${themoviedb.backdropPath}'
+          : 'https://programacion.net/files/article/20161110041116_image-not-found.png',
+      genreIds: themoviedb.genres.map((e) => e.name).toList(),
+      id: themoviedb.id,
+      originalLanguage: themoviedb.originalLanguage,
+      originalTitle: themoviedb.originalTitle,
+      overview: themoviedb.overview,
+      popularity: themoviedb.popularity,
+      posterPath: themoviedb.posterPath != ''
+          ? 'https://image.tmdb.org/t/p/w500${themoviedb.posterPath}'
+          : 'https://ih1.redbubble.net/image.1893341687.8294/fposter,small,wall_texture,product,750x1000.jpg',
       releaseDate: themoviedb.releaseDate,
       title: themoviedb.title,
       video: themoviedb.video,
